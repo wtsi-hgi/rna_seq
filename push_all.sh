968de9f5f71567db6875c43afc0dc0df02b120ff
gn5 <gn5@sanger.ac.uk>
2020-11-12 17:25:50 +0000

    auto

--- push_all.sh
+++ push_all.sh
@@ -1,2 +1,2 @@
 git add * && git commit -m "auto" && git push -u origin
-git add .gitlab-ci.yml && git commit -m "auto" && git push -u origin
+git add .github/* && git commit -m "auto" && git push -u origin
