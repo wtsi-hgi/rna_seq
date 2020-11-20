4b907fe00a2fd0a302d660fab6ab9c2340fabb2d
mercury <mercury@sanger.ac.uk>
2020-11-20 16:17:38 +0000

    auto_github

---
+++ ci_kill_and_run.sh
@@ -0,0 +1,3 @@
+git add * && git add .github/* && \
+  git commit --allow-empty -m "auto_gitlab; ci_run_kill; ci_run_lsf" && \
+  git push -u origin
---
+++ ci_run_lsf.sh
@@ -0,0 +1,1 @@
+git add * && git add .github/* && git commit --allow-empty -m "auto_github; ci_run_lsf" && git push -u origin
