60d289bb380d571bf3278306cd9a54baa459ca11
Guillaume G Noell <gn5@sanger.ac.uk>
2021-04-26 10:31:00 +0100

    auto-git

---
+++ build_indexes/build_salmon_index_cdna_plus_ncrna.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+eval "$(conda shell.bash hook)"
+conda activate /lustre/scratch118/humgen/resources/conda/star
+
+salmon index \
+-t Homo_sapiens.GRCh38.ncrna_plus_cdna.fa \
+-i salmon_index_Homo_sapiens.GRCh38.cdna_plus_ncrna.all \
+-k 31
+
